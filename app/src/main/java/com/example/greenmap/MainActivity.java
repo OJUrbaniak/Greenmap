c1198e6dd14cf51e5ee61d2b9016ccbdf1cc19ba
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-12 17:51:57 +0100

    Merging my changes

--- app/src/main/java/com/example/greenmap/MainActivity.java
+++ app/src/main/java/com/example/greenmap/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.greenmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
 
 import android.content.Intent;
@@ -30,4 +31,60 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private class PointOfInterest {
+        //Base point of interest class for types to expand upon
+        String id; //Set as data is pulled from db, used to keep track of POIs
+        String name;
+        String desc;
+        int type;
+        int carbonSaved; //Will have to decide what this is for different POIs and whether it will change over time
+        int reviewRating;
+
+        double lat;
+        double lon;
+        int distanceFromUser; //This gets set later
+
+        PointOfInterest(String name, String desc, Double lat, Double lon, int type, int reviewRating) {
+            this.name = name;
+            this.desc = desc;
+            this.lat = lat;
+            this.lon = lon;
+            this.type = type;
+            this.reviewRating = reviewRating;
+        }
+    }
+
+    private class WaterFountainPOI extends PointOfInterest {
+
+        boolean safeToDrinkStraight;
+        boolean bottleFilling;
+        boolean filtered;
+
+        WaterFountainPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating, int carbonSaved, boolean safeToDrinkStraight, boolean bottleFilling, boolean filtered) {
+            super(name, desc, lat, lon, type, reviewRating);
+            this.carbonSaved = carbonSaved;
+            this.safeToDrinkStraight = safeToDrinkStraight;
+            this.bottleFilling = bottleFilling;
+            this.filtered = filtered;
+        }
+    }
+
+    private class BikeRackPOI extends PointOfInterest {
+
+        boolean covered;
+
+        BikeRackPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating) {
+            super(name, desc, lat, lon, type, reviewRating);
+        }
+    }
+
+    private class RecyclingBinPOI extends PointOfInterest {
+
+        String binType;
+
+        RecyclingBinPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating, String binType) {
+            super(name, desc, lat, lon, type, reviewRating);
+            this.binType = binType;
+        }
+    }
 }
