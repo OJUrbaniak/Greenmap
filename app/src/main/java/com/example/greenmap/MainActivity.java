5a99afe6f34a07f04483d2d7a13c68e6d91186eb
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-12 16:27:16 +0100

    Implemented WaterFountainPOI, BikeRackPOI and RecyclingBinPOI based on the following from Datastructures file:
    POI (this is stored in the R tree) (not need in front end?)
      LocationLongitude Double
      LocationLatitude Double
      Type Int
      POI_ID Int
    
    Water Fountain POI
      POI_ID Int
      Review_Rating Float
      Name String
      Carbon Saved Int
      Description Long string
      Drink Straight Tap Boolean
      Bottle Filling Tap Boolean
      Filtered Boolean
    
    Bike Rack POI
      POI_ID Int
      Review_Rating Float
      Name String
      Carbon Saved Int
      Description Long string
      Covered Boolean
    
    Recycling Bin POI
      POI_ID Int
      Review_Rating Float
      Name String
      Carbon Saved Int
      Description Long string
      Recycling Bin Type String (must be one of set option from drop down box) DB dont worry this will be implemented at front end

--- app/src/main/java/com/example/greenmap/MainActivity.java
+++ app/src/main/java/com/example/greenmap/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.greenmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Point;
 import android.os.Bundle;
 
 import android.content.Intent;
@@ -32,12 +33,58 @@ public class MainActivity extends AppCompatActivity {
 
     private class PointOfInterest {
         //Base point of interest class for types to expand upon
-        int x;
-        int y;
-        int distanceFromUser; //This gets set later
-
-        String id;
+        String id; //Set as data is pulled from db, used to keep track of POIs
         String name;
         String desc;
+        int type;
+        int carbonSaved; //Will have to decide what this is for different POIs and whether it will change over time
+        int reviewRating;
+
+        double lat;
+        double lon;
+        int distanceFromUser; //This gets set later
+
+        public PointOfInterest(String name, String desc, Double lat, Double lon, int type, int reviewRating) {
+            this.name = name;
+            this.desc = desc;
+            this.lat = lat;
+            this.lon = lon;
+            this.type = type;
+            this.reviewRating = reviewRating;
+        }
+    }
+
+    private class WaterFountainPOI extends PointOfInterest {
+
+        boolean safeToDrinkStraight;
+        boolean bottleFilling;
+        boolean filtered;
+
+        public WaterFountainPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating, int carbonSaved, boolean safeToDrinkStraight, boolean bottleFilling, boolean filtered) {
+            super(name, desc, lat, lon, type, reviewRating);
+            this.carbonSaved = carbonSaved;
+            this.safeToDrinkStraight = safeToDrinkStraight;
+            this.bottleFilling = bottleFilling;
+            this.filtered = filtered;
+        }
+    }
+
+    private class BikeRackPOI extends PointOfInterest {
+
+        boolean covered;
+
+        public BikeRackPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating) {
+            super(name, desc, lat, lon, type, reviewRating);
+        }
+    }
+
+    private class RecyclingBinPOI extends PointOfInterest {
+
+        String binType;
+
+        public RecyclingBinPOI(String name, String desc, Double lat, Double lon, int type, int reviewRating, String binType) {
+            super(name, desc, lat, lon, type, reviewRating);
+            this.binType = binType;
+        }
     }
 }
