1c47c0d1f06cf4836ca384e8be7e690da5966ebb
milesfreemantle <miles.freemantle@hotmail.co.uk>
2020-04-13 14:58:05 +0100

    Added profile activity

--- app/src/main/java/com/example/greenmap/Profile.java
+++ app/src/main/java/com/example/greenmap/Profile.java
@@ -3,6 +3,8 @@ package com.example.greenmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void goToTOS(View view){
+        Intent intent = new Intent(this,TermsOfServiceActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToPP(View view){
+        Intent intent = new Intent(this,PrivacyPolicyActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToAU(View view){
+        Intent intent = new Intent(this,AboutUsActivity.class);
+        startActivity(intent);
     }
 }
