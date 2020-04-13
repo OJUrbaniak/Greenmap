b138c0c8e4c2e2bce6fe60b90fdabf52ec07f86e
milesfreemantle <miles.freemantle@hotmail.co.uk>
2020-04-13 20:05:46 +0100

    added create water activity

--- app/src/main/java/com/example/greenmap/CreateWaterActivity.java
+++ app/src/main/java/com/example/greenmap/CreateWaterActivity.java
@@ -3,6 +3,8 @@ package com.example.greenmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 
 public class CreateWaterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class CreateWaterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_water);
+    }
+
+    public void backToProfile(View view){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
     }
 }
