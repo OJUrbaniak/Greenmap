5fc84c45794c49e097c01c48bc73da297d7a202a
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-15 01:33:51 +0100

    Linked buttons, TextViews and EditText widgets to code in FiltersActivity and MainActivity

--- app/src/main/java/com/example/greenmap/FiltersActivity.java
+++ app/src/main/java/com/example/greenmap/FiltersActivity.java
@@ -3,12 +3,28 @@ package com.example.greenmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FiltersActivity extends AppCompatActivity {
+
+    boolean covered = false;       //defaults
+    int rackRange = 0;             //defaults
+    int rackMinRating = 0;         //defaults
+
+    boolean drinkingTap = false;   //defaults
+    boolean bottleRefill = false;  //defaults
+    int tapRange = 100000000;      //defaults
+    int tapMinRating = 0;          //defaults
+
+    TextView numberPOI;
+    Button saveFiltersButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
+        numberPOI = findViewById(R.id.textView16);
+        saveFiltersButton = findViewById(R.id.button4);
     }
 }
--- app/src/main/java/com/example/greenmap/MainActivity.java
+++ app/src/main/java/com/example/greenmap/MainActivity.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.view.View;
 
 import android.graphics.Point;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userNameField;
+    EditText passwordField;
+    Button loginButton;
+    //Button signupButton;     -- a method already exists for this
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameField = findViewById(R.id.editText);
+        passwordField = findViewById(R.id.editText2);
+        loginButton = findViewById(R.id.button2);
+        //signupButton = findViewById(R.id.button);
     }
 
     public void goToMap(View view){
