c53e51c1386a5a287f0c6e76a553361edd1b7a89
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-16 01:23:09 +0100

    Implemented the saving & loading of filters via a Preferences object which gets converted into json and gets saved in SharedPreferences. The activity tries to load previous preferences in onCreate(), using the default constructor of Preferences in case there are none set yet (e.g. if the user is launching app for the first time).
    -- Not tested yet because my computer is complaining about space for AVD

--- app/src/main/java/com/example/greenmap/FiltersActivity.java
+++ app/src/main/java/com/example/greenmap/FiltersActivity.java
@@ -2,29 +2,124 @@ package com.example.greenmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+
+import com.google.gson.Gson;
 
 public class FiltersActivity extends AppCompatActivity {
 
-    boolean covered = false;       //defaults
-    int rackRange = 0;             //defaults
-    int rackMinRating = 0;         //defaults
-
-    boolean drinkingTap = false;   //defaults
-    boolean bottleRefill = false;  //defaults
-    int tapRange = 100000000;      //defaults
-    int tapMinRating = 0;          //defaults
-
     TextView numberPOI;
-    Button saveFiltersButton;
+    //Button saveFiltersButton;
+    CheckBox rackCovered;
+    TextView rackRange;
+    TextView rackMinRating;
+    CheckBox drinkingTap;
+    CheckBox tapBottleRefill;
+    TextView tapRange;
+    TextView tapMinRating;
+
+    SharedPreferences pref = this.getSharedPreferences("com.example.greenmap", Context.MODE_PRIVATE);
+    Preferences userPref = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filters);
+        rackCovered = findViewById(R.id.checkBox);
+        rackRange = findViewById(R.id.textView7);
+        rackMinRating = findViewById(R.id.textView2);
+        drinkingTap = findViewById(R.id.checkBox2);
+        tapBottleRefill = findViewById(R.id.checkBox3);
+        tapRange = findViewById(R.id.textView14);
+        tapMinRating = findViewById(R.id.textView15);
+
+        loadPreferences();
+
         numberPOI = findViewById(R.id.textView16);
-        saveFiltersButton = findViewById(R.id.button4);
+        //saveFiltersButton = findViewById(R.id.button4);
+    }
+
+    public void savePreferences(View view) {
+        Preferences userPref = new Preferences(
+                rackCovered.isChecked(),
+                rackRange.getText().toString(),
+                rackMinRating.getText().toString(),
+                drinkingTap.isChecked(),
+                tapBottleRefill.isChecked(),
+                tapRange.getText().toString(),
+                tapMinRating.getText().toString()
+        );
+        SharedPreferences.Editor prefsEdit = pref.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(userPref);
+        prefsEdit.putString("userPref", json);
+        prefsEdit.commit();
+    }
+
+    private void loadPreferences() {
+        Gson gson = new Gson();
+        if (pref.contains("userPref")) {
+            String json = pref.getString("userPref","");
+            Preferences deviceUserPref = gson.fromJson(json, Preferences.class);
+            userPref = deviceUserPref;
+        }
+        else {
+            Log.d("PREF-LOAD","No preferences detected");
+            userPref = new Preferences();
+        }
+        showPreferences(userPref);
+    }
+
+    private void showPreferences(Preferences p) {
+        rackCovered.setChecked(p.rackCovered);
+        rackRange.setText(p.rackRange);
+        rackMinRating.setText(p.rackMinRating);
+        drinkingTap.setChecked(p.drinkingTap);
+        tapBottleRefill.setChecked(p.tapBottleRefill);
+        tapRange.setText(p.tapRange);
+        tapMinRating.setText(p.tapMinRating);
+    }
+
+    public class Preferences {
+        boolean rackCovered;
+        int rackRange;
+        int rackMinRating;
+        boolean drinkingTap;
+        boolean tapBottleRefill;
+        int tapRange;
+        int tapMinRating;
+
+        public Preferences() {
+            this.rackCovered = false;       //defaults
+            this.rackRange = 0;             //defaults
+            this.rackMinRating = 0;         //defaults
+            this.drinkingTap = false;       //defaults
+            this.tapBottleRefill = false;   //defaults
+            this.tapRange = 500;            //defaults
+            this.tapMinRating = 0;          //defaults
+        }
+
+        public Preferences(boolean rackCovered, String rackRange, String rackMinRating, boolean drinkingTap, boolean tapBottleRefill, String tapRange, String tapMinRating) {
+            try {
+                this.rackCovered = rackCovered;
+                this.rackRange = 0; try { Integer.parseInt(rackRange); } catch (NumberFormatException ex) {
+                    Log.e("PrefError","Rackrange not a number"); };
+                this.rackMinRating = 0; try { Integer.parseInt(rackMinRating); } catch (NumberFormatException ex) {Log.e("PrefError","rackMinRating not a number");};
+                this.drinkingTap = false;
+                this.tapBottleRefill = false;
+                this.tapRange = 500; try { Integer.parseInt(tapRange); } catch (NumberFormatException ex) {Log.e("PrefError","tapRange not a number");};
+                this.tapMinRating = 0; try { Integer.parseInt(tapMinRating); } catch (NumberFormatException ex) {Log.e("PrefError","tapMinRating not a number");};
+            }
+            catch (Exception ex) {
+                Log.e("PrefError","ONE OR MORE SETTINGS NOT INTS");
+            }
+        }
     }
 }
