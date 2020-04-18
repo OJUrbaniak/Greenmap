73586acf20abf06d1a148a08859604a0e5a0b303
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-19 00:26:54 +0100

    ViewPOI works on NearbyListActivity

--- app/src/main/java/com/example/greenmap/NearbyListActivity.java
+++ app/src/main/java/com/example/greenmap/NearbyListActivity.java
@@ -24,13 +24,20 @@ public class NearbyListActivity extends AppCompatActivity {
         PointOfInterest[] data = new PointOfInterest[]{};
         data = (PointOfInterest[]) getIntent().getSerializableExtra("dataArray");
         for (int i = 0; i < data.length; i++) {
-            PointOfInterest currItem = data[i];
+            final PointOfInterest currItem = data[i];
             Log.d("ViewPOI","NEARBY Item "+i+": "+currItem.name+" lat "+currItem.getCoords().latitude+" lon "+currItem.getCoords().longitude);
             TableRow tr = new TableRow(this);
             TextView name = new TextView(this); name.setText(data[i].name);
             TextView desc = new TextView(this); desc.setText(data[i].desc);
             Button viewButton = new Button(this);
             viewButton.setText("View POI");
+            viewButton.setText("View POI");
+            viewButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    goToViewPOI(currItem);
+                }
+            });
             tr.addView(name);
             tr.addView(desc);
             tr.addView(viewButton);
@@ -40,5 +47,11 @@ public class NearbyListActivity extends AppCompatActivity {
 
     public void backToMap(View view){
         finish();
+    }
+
+    public void goToViewPOI(PointOfInterest currentPOI){
+        Intent intent = new Intent(this,ViewPOIActivity.class);
+        intent.putExtra("currentPOI", currentPOI);
+        startActivity(intent);
     }
 }
