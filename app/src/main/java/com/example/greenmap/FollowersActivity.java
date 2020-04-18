6d703598e9fcc74bcc9d18b846c70d82a974c4be
Joe <joebilly8@hotmail.co.uk>
2020-04-18 21:46:23 +0100

    Updated the look for the TOS and Privacy Policy UI

--- app/src/main/java/com/example/greenmap/FollowersActivity.java
+++ app/src/main/java/com/example/greenmap/FollowersActivity.java
@@ -1,8 +1,9 @@
 package com.example.greenmap;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class FollowersActivity extends AppCompatActivity {
 
