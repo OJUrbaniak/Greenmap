1603eace4bea18961d9684b3839d81375e559ed0
OJUrbaniak <42911055+ojurbaniak@users.noreply.github.com>
2020-04-22 18:24:37 +0100

    Updated Filter validation

--- app/src/main/java/com/example/greenmap/FiltersActivity.java
+++ app/src/main/java/com/example/greenmap/FiltersActivity.java
@@ -39,7 +39,6 @@ public class FiltersActivity extends AppCompatActivity {
     Preferences userPref = null;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +61,7 @@ public class FiltersActivity extends AppCompatActivity {
 
         //Get user from login
         Intent i = getIntent();
-        user = (User)i.getSerializableExtra("User");
+        user = (User) i.getSerializableExtra("User");
 
         loadPreferences();
 
@@ -71,58 +70,67 @@ public class FiltersActivity extends AppCompatActivity {
 
     public void savePreferences(View view) {    //CALLED BY SAVE FILTERS BUTTON
         String rangeText = range.getText().toString();
-        int rangeValue = Integer.parseInt(rangeText);
-        if((rangeValue <= 500 && rangeValue >= 1) && (Integer.parseInt(minRating.getText().toString()) <= 5 && Integer.parseInt(minRating.getText().toString()) > 0)){
-            warningTextLabel.setText("");
-            Log.d("PREF-LOAD","Save button hit trying to save prefs");
-            pref = getSharedPreferences("com.example.greenmap", 0);
+        String minRatingText = minRating.getText().toString();
+//        int minRatingValue = 1;
+//        int rangeValue = 500;
+        try {
+            int rangeValue = Integer.parseInt(rangeText);
+            if (!(rangeValue <= 500 && rangeValue >= 1))
+                throw new Exception("Please enter a range between 1-500");
             try {
-                userPref = new Preferences(
-                        showRacks.isChecked(),
-                        showTaps.isChecked(),
-                        showBins.isChecked(),
-                        rackCovered.isChecked(),
-                        drinkingTap.isChecked(),
-                        tapBottleRefill.isChecked(),
-                        tapBottleFiltered.isChecked(),
-                        Integer.parseInt(range.getText().toString()),
-                        Integer.parseInt(minRating.getText().toString())
-                );
-            }
-            catch (NumberFormatException e) {
-                Log.d("SavePreferences","Parsing error");
-            }   
-            SharedPreferences.Editor prefsEdit = pref.edit();
-            Gson gson = new Gson();
-            String json = gson.toJson(userPref);
-            prefsEdit.putString("userPref", json);
-            prefsEdit.commit();
-            savedLabel.setVisibility(View.VISIBLE);
+                int minRatingValue = Integer.parseInt(minRatingText);
+                if (!(minRatingValue <= 5 && minRatingValue > 0))
+                    throw new Exception("Please enter a minimum rating between 0-5");
+                warningTextLabel.setText("");
+                Log.d("PREF-LOAD", "Save button hit trying to save prefs");
+                pref = getSharedPreferences("com.example.greenmap", 0);
+                try {
+                    userPref = new Preferences(
+                            showRacks.isChecked(),
+                            showTaps.isChecked(),
+                            showBins.isChecked(),
+                            rackCovered.isChecked(),
+                            drinkingTap.isChecked(),
+                            tapBottleRefill.isChecked(),
+                            tapBottleFiltered.isChecked(),
+                            rangeValue,
+                            minRatingValue
+                    );
+                } catch (NumberFormatException e) {
+                    Log.d("SavePreferences", "Parsing error");
+                }
 
-            //Go back to map
-            setResult(RESULT_OK, null);
-            finish();
-//            Intent intent = new Intent(FiltersActivity.this, MapActivity.class);
-//            intent.putExtra("User", (Parcelable) user);
-//            startActivity(intent);
-        } else {
-            Log.i("warning Label", "out of range");
-            warningTextLabel.setText("Please enter a range between 1-500 and a minimum rating between 0-5");
-            range.setText("1");
+                SharedPreferences.Editor prefsEdit = pref.edit();
+                Gson gson = new Gson();
+                String json = gson.toJson(userPref);
+                prefsEdit.putString("userPref", json);
+                prefsEdit.commit();
+                savedLabel.setVisibility(View.VISIBLE);
+
+                //Go back to map
+                setResult(RESULT_OK, null);
+                finish();
+            }
+            catch (Exception e) {
+                warningTextLabel.setText("Please enter a minimum rating between 0-5");
+                minRating.setText("1");
+            }
+        } catch (Exception e) {
+            warningTextLabel.setText("Please enter a range between 1-500");
+            range.setText("500");
         }
     }
 
     private void loadPreferences() {
         pref = getSharedPreferences("com.example.greenmap", 0);
         if (pref.contains("userPref")) {
-            Log.d("PREF-LOAD","Preferences detected");
+            Log.d("PREF-LOAD", "Preferences detected");
             Gson gson = new Gson();
-            String json = pref.getString("userPref","");
+            String json = pref.getString("userPref", "");
             Preferences deviceUserPref = gson.fromJson(json, Preferences.class);
             userPref = deviceUserPref;
-        }
-        else {
-            Log.d("PREF-LOAD","No preferences detected");
+        } else {
+            Log.d("PREF-LOAD", "No preferences detected");
             userPref = new Preferences();
         }
         showPreferences(userPref);
@@ -147,7 +155,7 @@ public class FiltersActivity extends AppCompatActivity {
         if ((tapBottleRefill.isChecked() == true && p.tapBottleRefill == false) || (tapBottleRefill.isChecked() == false && p.tapBottleRefill == true)) {
             tapBottleRefill.toggle();
         }
-        if((tapBottleFiltered.isChecked() == true && p.tapFiltered == false) || (tapBottleFiltered.isChecked() == false && p.tapFiltered == true)){
+        if ((tapBottleFiltered.isChecked() == true && p.tapFiltered == false) || (tapBottleFiltered.isChecked() == false && p.tapFiltered == true)) {
             tapBottleFiltered.toggle();
         }
         range.setText(Integer.toString(p.range));
